194202766b2babc18d3005b65bccf60d1b03f6f6
Walter Sotil <waltersotil@gmail.com>
2020-06-30 11:01:56 -0300

    Le aumenté la velocidad al player

--- Player.gd
+++ Player.gd
@@ -13,13 +13,13 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		velocity.x += 1*2
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+		velocity.x -= 1*2
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		velocity.y += 1*2
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+		velocity.y -= 1*2
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
